28088bea49757223fea51a519e32fad9d831ea20
Ashin Sabu <ashin.sabu3@gmail.com>
2021-07-25 13:55:56 +0530

    Added comments to C++ macros file for better understanding

--- Macros/cpmacros.cpp
+++ Macros/cpmacros.cpp
@@ -2,6 +2,8 @@
 #include <bits/stdc++.h>
 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//disables synchronization between C and C++ std streams(cannot mix C and C++ style I/O's with usage of this)
+//cin tie null and cout tie null unties the default(use only when not using stuff like "Enter name: " and then taking input)
 #define vi vector<int>
 #define vll vector<long long>
 #define ALL(v) v.begin(), v.end()
@@ -14,5 +16,6 @@ using namespace std;
 int main()
 {
     IOS;  
-    //cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+    //cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+    //for printing runtime if using compiler/ide/editor without time feature
 }
