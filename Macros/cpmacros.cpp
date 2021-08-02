61c5f7d146907a9028fed75568150c1c6eb1e5d9
Ashin Sabu <ashin.sabu3@gmail.com>
2021-08-02 23:45:50 +0530

    Optional file for input and output in online judges

--- Macros/cpmacros.cpp
+++ Macros/cpmacros.cpp
@@ -15,6 +15,10 @@ using namespace std;
 
 int main()
 {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     IOS;  
     //cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
     //for printing runtime if using compiler/ide/editor without time feature
