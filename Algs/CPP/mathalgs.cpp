79c72b5d89caa3faa59999ba702f1789bf390f2a
Ashin Sabu <ashin.sabu3@gmail.com>
2021-08-07 00:20:31 +0530

    function for multiplying large numbers(100's of digits) using array

--- Algs/CPP/mathalgs.cpp
+++ Algs/CPP/mathalgs.cpp
@@ -1,7 +1,13 @@
 //Ashin Sabu - https://www.linkedin.com/in/ashin-sabu-1059a6175/ - https://github.com/ashinsabu
-
+// #include<bits/stdc++.h> 
+//compiles all headers
 #include <iostream>
+#include <conio.h>
+//to use the IOS function to disable sync with stdio
 #include <algorithm>
+//for __gcd(),
+#include <vector>
+//for stl vectors
 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define vi vector<int>
@@ -10,7 +16,21 @@
 #define SORT(v) sort(ALL(v))
 
 using namespace std;
-    
+void largemultiply(vector<int> &a,int &m,int n)
+//vector with digits arranged in reverse order, number of digits in vector, number to multiply by
+{
+    int temp=0;
+    for(int i=0;i<m;i++){
+        int x=(n*a[i]+temp);
+        a[i]=x%10;
+        temp=x/10;
+    }
+    while(temp){
+        a.push_back(temp%10);
+        temp/=10;
+        m++;
+    }
+}
 //fast calculate ncr(also has case for r>n printing 0)
 long long calcNcR(int n, int r)
 {
@@ -47,6 +67,7 @@ long long calcNcR(int n, int r)
  
     return p;
 }
+
 int main()
 {
     IOS;
