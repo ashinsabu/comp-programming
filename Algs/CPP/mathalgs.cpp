d3fdbd5508a8c75b03b84e3b4fbffe8cd6f808b4
Ashin Sabu <ashin.sabu3@gmail.com>
2021-09-05 17:44:07 +0530

    Collatz Conjecture test(veritasium video explains clearly)

--- Algs/CPP/mathalgs.cpp
+++ Algs/CPP/mathalgs.cpp
@@ -15,7 +15,28 @@
 #define ALL(v) v.begin(), v.end()
 #define SORT(v) sort(ALL(v))
 
+typedef long long ll;
 using namespace std;
+void collatzconjecturetest(){
+    ll nn; ll operations=0;
+    int a=0;
+    for(nn=5;nn<10000000;nn++)
+    {
+        ll n=nn;
+        while(n!=1){
+            if(n%2==0){
+                operations++;
+                n=n/2;
+            }
+            else if(n%2==1){
+                n=(n*3)+1; operations++;
+            }
+        }
+        if(n==1) continue;
+        else a++;
+    }
+    cout<<a<<endl<<operations;//always comes out to 1 for every number, operations is to find operations per second of cpu
+}
 void largemultiply(vector<int> &a,int &m,int n)
 //vector with digits arranged in reverse order, number of digits in vector, number to multiply by
 {
