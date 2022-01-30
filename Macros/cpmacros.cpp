ce5f8613890154b12b8c85f673de1a9aaeaacece
Ashin Sabu <ashin.sabu3@gmail.com>
2022-01-30 20:00:05 +0530

    reordered template and added debugging macros

--- Macros/cpmacros.cpp
+++ Macros/cpmacros.cpp
@@ -1,32 +1,38 @@
-//Ashin Sabu - https://www.linkedin.com/in/ashin-sabu-1059a6175/ - https://github.com/ashinsabu
-//pragmas directive allows use of suppressed optimizations available
-//only in g++ compilers(won't work on gcc compilers)
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 
 #include <bits/stdc++.h>
 
+using namespace std;
+
+
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-//disables synchronization between C and C++ std streams(cannot mix C and C++ style I/O's with usage of this)
-//cin tie null and cout tie null unties the default(use only when not using stuff like "Enter name: " and then taking input)
 #define vi vector<int>
 #define vll vector<long long>
 #define ALL(v) v.begin(), v.end()
 #define SORT(v) sort(ALL(v))
-//other small preprocessor directives to code faster ^^
-
 typedef long long ll;
+const unsigned ll MOD=1e9 + 7;
 
-using namespace std;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int a){cerr << a;} void _print(long long a){cerr << a;} void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;}
+template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
     IOS;  
-    //cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
-    //for printing runtime if using compiler/ide/editor without time feature
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
 }
