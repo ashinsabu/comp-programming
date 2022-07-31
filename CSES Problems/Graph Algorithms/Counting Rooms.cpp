043f80bba1c733a4932ec89e27d55639fb4d1cd5
ashinsabu <ashin.sabu3@gmail.com>
2022-07-31 15:28:38 +0530

    Removed unwanted debugging code and added comments for understanding

--- CSES Problems/Graph Algorithms/Counting Rooms.cpp
+++ CSES Problems/Graph Algorithms/Counting Rooms.cpp
@@ -13,21 +13,6 @@ using namespace std;
 typedef long long ll;
 const int MOD = 1e9 + 7;
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;} 
-template<class T, class U> void _print(pair<T,U> p1){cerr<<"{"<<p1.first<<','<<p1.second<<"}";}
-template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
-template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
-template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
-template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
-template<class T,class U> void _print(unordered_map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
-template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
-
 int n,m;
 char a[1000][1000];
 vector<vector<bool>> visited(1000,vector<bool>(1000,false)); //2d visited vector for dfs
