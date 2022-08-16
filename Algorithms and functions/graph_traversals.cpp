62851a7dbfbded300e1277560e0dfeb2f6877492
ashinsabu <ashin.sabu3@gmail.com>
2022-08-16 09:04:44 +0530

    Added simple bfs

--- Algorithms and functions/graph_traversals.cpp
+++ Algorithms and functions/graph_traversals.cpp
@@ -5,6 +5,7 @@ using namespace std;
 const int N=8+1; // +1 to skip 0 index since 1 indexed graph
 vector<bool> vis={0,0,0,0,0,0,0,0,0};
 vector<int> adj[] = {{},{2},{1},{4},{3,5,6},{4,7},{4},{5},{}}; //1 indexed graph
+vector<int> dist(N=1);
 
 //  Recursive DFS for single connected component graph
 void dfs(int u){
@@ -27,7 +28,31 @@ void fulldfs(){
     }
 }
 
+void simplebfs(){
+ 
+    for(int i=0;i<n+1;i++)
+        dist[i]=INT_MAX;
+ 
+    dist[1]=1;
+    queue<int> q;
+    q.push(1);
+    parent[1] = -1;
+ 
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        for(int nextNode: adj[node]){            
+            if(dist[nextNode] == INT_MAX){
+                dist[nextNode] = dist[node]+1;
+                parent[nextNode] = node;
+                q.push(nextNode);
+            } 
+        }
+    }
+}
+
 int main(){    
     fulldfs();
     cout<<components;
+    
 }
