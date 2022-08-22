7bf80317c9ae2732ac544cc69ffcb047c5993ce1
ashinsabu <ashin.sabu3@gmail.com>
2022-08-22 21:42:50 +0530

    CSES Problem: Coin Combinations I

---
+++ CSES Problems/Dynamic Programming/Coin Combinations I.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;} 
+template<class T, class U> void _print(pair<T,U> p1){cerr<<"{"<<p1.first<<','<<p1.second<<"}";}
+template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(unordered_set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(unordered_map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+
+
+int n,x;
+vi a;
+vi dp(1000001,-1);
+
+int solve(int total){
+    if(total == 0)
+        return 1;
+
+    if(total<0)
+        return 0;
+
+    if(dp[total]!=-1)
+        return dp[total];
+
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        ans += solve(total-a[i])%MOD;
+    }
+
+    return dp[total] = ans%MOD;
+}
+
+int32_t main()
+{
+    cin>>n>>x;
+    a.resize(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    int ans = solve(x);
+
+    cout<<ans%MOD;
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    
+}   
+
+/*dont use ceil*/
