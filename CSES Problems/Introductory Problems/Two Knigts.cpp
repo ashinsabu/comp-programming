41f5d94b5f9320dffa52375fc53b05523e30ce8c
ashinsabu <ashin.sabu3@gmail.com>
2023-03-16 01:51:12 +0530

    CSES Problms: 1. Coin Piles 2. Two Knights

---
+++ CSES Problems/Introductory Problems/Two Knigts.cpp
@@ -0,0 +1,86 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+ 
+#define int long long int
+ 
+typedef long long ll;
+const unsigned long long MOD = 998244353;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;}
+template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+ 
+ll nCr(int n, int r)
+{
+ 
+    // p holds the value of n*(n-1)*(n-2)...,
+    // k holds the value of r*(r-1)...
+    long long p = 1, k = 1;
+ 
+    // C(n, r) == C(n, n-r),
+    // choosing the smaller value
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            // gcd of p, k
+            long long m = __gcd(p, k);
+ 
+            // dividing by gcd, to simplify
+            // product division by their gcd
+            // saves from the overflow
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+        // k should be simplified to 1
+        // as C(n, r) is a natural number
+        // (denominator should be 1 ) .
+    }
+ 
+    else
+        p = 1;
+ 
+    return p;
+}
+int32_t main()
+{
+    IOS;
+    int n;
+    cin>>n;
+    // cout<<nCr(16,2)-4*3*2;
+    for(int i=1;i<=n;i++){
+        if(i==1)
+            cout<<0<<endl;
+        
+        else
+            cout<<nCr((i*i),2) - 4*(i-1)*(i-2)<<endl;
+    }
+}
