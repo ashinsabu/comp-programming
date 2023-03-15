6f488af3603ae72325ebcba23fb22b859ab7da40
ashinsabu <ashin.sabu3@gmail.com>
2023-03-16 01:36:37 +0530

    CSES Problem: Trailing Zeroes

---
+++ CSES Problems/Introductory Problems/Trailing Zeroes.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;} 
+template<class T, class U> void _print(pair<T,U> p1){cerr<<"{"<<p1.first<<','<<p1.second<<"}";}
+template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(unordered_set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(unordered_map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+
+
+void solve(){
+    int n;
+    cin>>n;
+    int cnttwo = 0,cntfive = 0; // number of times 2 and 5 occurs as prime factors along with counts
+    int temp = n,curPow = 1;
+    while(pow(2,curPow)<=temp){
+        cnttwo += temp/(pow(2,curPow));
+        curPow++;
+    }
+    temp = n,curPow = 1;
+    while(pow(2,curPow)<=temp){
+        cntfive += temp/(pow(5,curPow));
+        curPow++;
+    }
+    cout<<min(cntfive,cnttwo);
+}
+
+int32_t main()
+{
+    IOS;
+    int tt = 1;
+    // cin>>tt;
+    while(tt--){
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+}   
+
