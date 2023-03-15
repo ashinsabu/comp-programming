77f94c30b22322136296f617cab1f7f65a2dad38
ashinsabu <ashin.sabu3@gmail.com>
2023-03-16 01:55:54 +0530

    Remove unnecessary files and update template

--- template.cpp
+++ template.cpp
@@ -1,23 +1,14 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
+#define int long long int
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define vi vector<int>
 #define vll vector<long long>
 #define ALL(v) v.begin(), v.end()
 #define SORT(v) sort(ALL(v))
-
-#define int long long int
-
 typedef long long ll;
-
-const unsigned long long MOD=998244353;
+const int MOD = 1e9 + 7;
 
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
@@ -25,21 +16,36 @@ const unsigned long long MOD=998244353;
 #define debug(x)
 #endif
 
-void _print(int a){cerr << a;} void _print(long long a){cerr << a;} void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;}
+void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;} 
 template<class T, class U> void _print(pair<T,U> p1){cerr<<"{"<<p1.first<<','<<p1.second<<"}";}
 template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
 template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(unordered_set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
 template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
 template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(unordered_map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
 template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
 
+void solve(){
+    
+}
 
 int32_t main()
 {
     IOS;
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout); 
-    #endif  
-    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
-}
+    int tt = 1;
+    cin>>tt;
+    while(tt--){
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+}   
+
+
+/*
+I'm amazing, born on a full moon 
+I was bred to get it in, no spoon 
+That's why I'm so gone, 
+summertime, in June
+*/
+
