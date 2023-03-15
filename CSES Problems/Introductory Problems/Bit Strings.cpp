781f9ac63973f0ad7f514a55e6497a5c12354173
ashinsabu <ashin.sabu3@gmail.com>
2023-03-16 01:25:41 +0530

    CSES Problem: Bit Strings

---
+++ CSES Problems/Introductory Problems/Bit Strings.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+#define vll vector<long long>
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int a){cerr << a;} /*void _print(long long a){cerr << a;}*/ void _print(char a){cerr << a;} void _print(string a){cerr << a;} void _print(bool a){cerr << a;} 
+template<class T, class U> void _print(pair<T,U> p1){cerr<<"{"<<p1.first<<','<<p1.second<<"}";}
+template<class T> void _print(vector <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(unordered_set <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T> void _print(multiset <T> v1){cerr << "[ "; for(T i:v1){_print(i); cerr << " "; } cerr << "]";}
+template<class T,class U> void _print(map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(unordered_map <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+template<class T,class U> void _print(multimap <T,U> v1){cerr << "[ "; for(auto& i:v1){_print(i.first); cerr << "-"; _print(i.second); cerr <<" , "; } cerr << "]";}
+
+int pow(int x, int y, int m){
+    int res = 1;
+
+    x=x%m;
+    if(!x) return x;
+
+    while(y>0){
+        if(y&1)
+            res = (res*x)%m;
+        y = y>>1;
+        x = (x*x)%m;
+    }
+    return res;
+}
+void solve(){
+    int n;
+    cin>>n;
+    cout<<pow(2,n,MOD);
+}
+
+int32_t main()
+{
+    IOS;
+    int tt = 1;
+    // cin>>tt;
+    while(tt--){
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+}   
+
+
